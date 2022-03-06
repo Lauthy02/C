70d34b1130869a057ebbf7a0ceedc72521d647dc
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-03-06 16:36:34 -0300

    Create U7.c

---
+++ U7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL,"spanish");
+    int a,b,c;
+    printf("Ingrese dos números enteros que quiera multiplicar con el formato \"a,b\": ");
+    scanf("%i,%i",&a,&b);
+    c=a*b;
+    printf("El resultado es: %i \n",c);
+    system("pause");
+    return 0;
+}
