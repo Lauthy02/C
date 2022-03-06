9581949a1cf319185c02d943b0825759bd2170b7
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-03-06 16:41:40 -0300

    Create U9.c

---
+++ U9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL,"spanish");
+    float base;
+    float indice;
+    printf("Ingrese el número que quiera hacer la raíz: ");
+    scanf("%f",&base);
+    printf("Ingrese el índice de la raíz: ");
+    scanf("%f",&indice);
+    printf("La raíz %.2f de %.2f da como resultado: %.3f \n\n",indice,base,pow(base,(1.0/indice)));
+    //la función pow hace lo de elevar un número por otro
+    system("pause");
+    return 0;
+}
