30cd3ae5666431f4439c51c9121e3cc249a45266
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-03-08 19:14:43 -0300

    Update and rename U3.c to U31.c

--- U31.c
+++ U31.c
@@ -1,5 +1,5 @@
 /*
-U31 Un vendedor recibe un sueldo base, más un 10% extra por comisión de sus ventas,
+U31. Un vendedor recibe un sueldo base, más un 10% extra por comisión de sus ventas,
 el vendedor desea saber cuánto dinero obtendrá por concepto de comisiones por
 las ventas que realiza en el mes y el total que recibirá en el mes tomando
 en cuenta su sueldo base y comisiones.
