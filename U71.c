c1389608975be92cfcb8e6154569c32e319e06cb
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-03-12 00:06:21 -0300

    Create U71.c

---
+++ U71.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+U71. Desarrollar un programa que permita ingresar dos números enteros y mostrarlos por pantalla de menor a mayor.
+    2 valores: ab ba
+*/
+
+int main()
+{
+    setlocale(LC_ALL,"spanish"); system("color F0");
+    int a=0, b=0;
+    char a2;
+
+    do
+    {
+        system("cls");
+
+        printf("Ingrese el 1er valor: ");
+        scanf("%i",&a);
+        fflush(stdin);
+        printf("\nIngrese el 2do valor: ");
+        scanf("%i",&b);
+        fflush(stdin);
+
+        if(a<b) //ab
+        {
+            printf("\n\nMenor a mayor: %i, %i",a,b);
+        }
+        else if(b<a) //ba
+        {
+            printf("\n\nMenor a mayor: %i, %i",b,a);
+        }
+
+        printf("\n\n\nSi desea salir presione s, si desea continuar presione c. \n\n");
+        scanf("%c",&a2);
+        fflush(stdin);
+    }while(a2=='c');
+    return 0;
+}
